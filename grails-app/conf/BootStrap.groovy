fd1f9a0332768e0b9fef45e5e262fc01c09023b0
susannej <susanne_j@gmx.de>
2011-12-28 20:52:31 +0100

    Added 2 default-user to the grails-bootstrap for the development environment

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,24 @@
+import de.susannej.urlaub.Employee
+
 class BootStrap {
 
     def init = { servletContext ->
+		environments {
+			development {
+        def admin = new Employee(loginname:"admin", 
+                             password:"admin", 
+                             firstname:"Administrator", 
+                             lastname:"admin",
+							 admin: 'Y')
+        admin.save()
+		
+        def jdoe = new Employee(loginname:"jdoe", 
+                            password:"jdoe", 
+                            firstname:"John Doe", 
+                            lastname:"author")
+        jdoe.save()
+        
+	  }
     }
-    def destroy = {
     }
 }
