08323cac5b351903cc72c511bc712f8dd9f60a7d
susannej <susanne_j@gmx.de>
2012-01-14 01:02:10 +0100

    modified the status default entries

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -7,37 +7,37 @@ class BootStrap {
     def init = { servletContext ->
 		environments {
 			development {
-        def admin = new Employee(loginname:"admin", 
-                             password:"admin", 
-                             firstname:"Administrator", 
-                             lastname:"admin",
-							 admin: true)
-        admin.save()
+				def admin = new Employee(loginname:"admin", 
+                             	password:"admin", 
+								 firstname:"Administrator", 
+								 lastname:"admin",
+								 admin: true)
+				admin.save()
 		
-        def jdoe = new Employee(loginname:"jdoe", 
-                            password:"jdoe", 
-                            firstname:"John", 
-                            lastname:"Doe")
-        jdoe.save()
+				def jdoe = new Employee(loginname:"jdoe", 
+                            	password:"jdoe", 
+								firstname:"John", 
+								lastname:"Doe")
+				jdoe.save()
 		
-		def susannej = new Employee(loginname: "susannej",
-							password: "susannej",
-							firstname: "Susanne",
-							lastname: "Jaeckel",
-							admin: true,
-							approve: true)
-		susannej.save()
+				def susannej = new Employee(loginname: "susannej",
+								password: "susannej",
+								firstname: "Susanne",
+								lastname: "Jaeckel",
+								admin: true,
+								approve: true)
+				susannej.save()
 		
-		(new Reason(description: "Urlaub", needsVacDesc: false)).save()
-		(new Reason(description: "Schulung", needsVacDesc: true)).save()
-		(new Reason(description: "Krank", needsVacDesc: false)).save()
+				(new Reason(description: "Urlaub", needsVacDesc: false)).save()
+				(new Reason(description: "Schulung", needsVacDesc: true)).save()	// blau
+				(new Reason(description: "Krank", needsVacDesc: false)).save()		// maroon
 		
-		(new Status(orderId: 1, description: "gemerkt", onlyApprover: false, displayColor: "yellow")).save()
-		(new Status(orderId: 2, description: "beantragt", onlyApprover: false, displayColor: "grey")).save()
-		(new Status(orderId: 3, description: "genehmigt", onlyApprover: true, displayColor: "green")).save()
-		(new Status(orderId: 4, description: "abgelehnt", onlyApprover: true, displayColor: "red")).save()
+				(new Status(orderId: 1, description: "notiert", onlyApprover: false, displayColor: "yellow")).save()
+				(new Status(orderId: 2, description: "beantragt", onlyApprover: false, displayColor: "red")).save()
+				(new Status(orderId: 3, description: "genehmigt", onlyApprover: true, displayColor: "green")).save()
+				(new Status(orderId: 4, description: "abgelehnt", onlyApprover: true, displayColor: "black")).save()
 		
-	  }
-    }
+			}
+		}
     }
 }
